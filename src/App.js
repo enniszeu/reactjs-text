9de0f483339e525d4323d82d5ca8bcd6f439e631
enniszeu <enniszeu@gmail.com>
2019-12-01 07:37:03 +0700

    upload-new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="row">
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        
+      </div>
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
